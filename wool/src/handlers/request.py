3f38c7a15e6a953251ec4158d3291e5fbeb43a55
Mark Snaith <msnaith@dundee.ac.uk>
2020-09-08 11:07:38 +0100

    Documentation

--- wool/src/handlers/request.py
+++ wool/src/handlers/request.py
@@ -5,6 +5,9 @@ import mongo
 
 @daf.message_handler("WOOL/requests", "WOOL/response")
 class WoolRequestHandler:
+    """
+    Handles messages sent to the WOOL/requests topic
+    """
 
     def __init__(self):
         self.server = "https://servletstest.rrdweb.nl/wool/v1/dialogue/"
@@ -13,6 +16,9 @@ class WoolRequestHandler:
 
     @daf.command_handler("new")
     def handle_new(self, command, data):
+        """
+        Handle "new" commands
+        """
         dialogueID = data.get("dialogueID", None)
         username = data.get("username", None)
         topic = data.get("topic", None)
@@ -27,7 +33,15 @@ class WoolRequestHandler:
         if dialogueID is not None and username is not None and topic is not None and participants is not None:
             authToken = user_data.get("authToken", None)
 
-            if authToken is None:
+            # get the "agent" participant
+            agent = None
+
+            for p in participants:
+                if p["player"].lower() == "agent":
+                    agent = p["name"].lower()
+                    break
+
+            if authToken is None or agent is None:
                 return {"error": "User is not authorised"}
 
             self.dialogue = {
@@ -36,7 +50,12 @@ class WoolRequestHandler:
                 "authToken": authToken,
                 "participants": {p["player"].lower(): p["name"] for p in participants}
             }
-            self.dialogue["moveData"] = self.start_dialogue()
+            move_data = self.start_dialogue(topic, agent)
+
+            if move_data is None:
+                return {}
+
+            self.dialogue["moveData"] = move_data
             self.save_dialogue()
 
             response = {"dialogueID": dialogueID,
@@ -51,6 +70,9 @@ class WoolRequestHandler:
 
     @daf.command_handler("moves", response_topic="WOOL/dialogue_moves")
     def handle_moves(self, command, data):
+        """
+        Handle "moves" command, overriding the response topic to be WOOL/dialogue_moves
+        """
         dialogueID = data.get("dialogueID", None)
         self.dialogue = self.load_dialogue(dialogueID)
 
@@ -61,6 +83,9 @@ class WoolRequestHandler:
 
     @daf.command_handler("interaction")
     def handle_interaction(self, command, data):
+        """
+        Handle "interaction" command
+        """
         dialogueID = data.get("dialogueID", None)
 
         to_return = {"moves": {}}
@@ -98,6 +123,14 @@ class WoolRequestHandler:
         return self.handle_moves("moves", data)
 
     def load_dialogue(self, dialogueID):
+        """
+        Load a dialogue with the given dialogueID
+
+        :param dialogueID: the dialogue ID to load
+        :type dialogueID: str
+        :return: the loaded dialogue
+        :rtype: dict
+        """
         if dialogueID is None:
             self.dialogue = {}
         else:
@@ -113,6 +146,18 @@ class WoolRequestHandler:
         result = col.replace_one({"dialogueID": dialogueID}, self.dialogue, upsert=True)
 
     def wool_request(self, action, post_data=None, qs=None):
+        """
+        Send a request to the Wool Web Service
+
+        :param action: the action to perform
+        :param post_data: optional post data to send to the action
+        :param qs optional query string parameters and values
+        :type action: str
+        :type post_data: dict
+        :type qs: dict
+        :return: the response from the request, or None on failure
+        :rtype: dict, or None on failure
+        """
         queryString = ""
 
         if qs is not None:
@@ -125,19 +170,56 @@ class WoolRequestHandler:
 
             headers = {"content-type": "application/json", "accept": "*/*", "X-Auth-Token": authToken}
             response = requests.post(url, headers=headers)
-            return json.loads(response.text)
+
+            if response.status_code == 200:
+                return json.loads(response.text)
+            else:
+                return None
         else:
             return None
 
-    def start_dialogue(self):
-        response = self.wool_request("start-dialogue", qs={"dialogueName": "carlos-social-introduction", "language":"en"})
-        return self.get_moves_from_response(response)
+    def start_dialogue(self, topic, agent):
+        """
+        Start a dialogue in the Wool Web Service
 
-    def progress_dialogue(self, dialogueID, moveID):
+        :param topic: the topic of the dialogue
+        :param agent: the name of the agent in the dialogue
+        :type topic: str
+        :type agent: str
+        :return: the moves available
+        :rtype: dict
+        """
+
+        # TODO: take topic and agent and select dialogue
+
+        response = self.wool_request("start-dialogue", qs={"dialogueName": "carlos-social-introduction", "language":"en"})
+
+        if response is not None:
+            return self.get_moves_from_response(response)
+        else:
+            return None
+
+    def progress_dialogue(self, moveID):
+        """
+        Progress the dialogue based on playing the given moveID
+
+        :param moveID: the ID of the move
+        :type moveID: int
+        :return: the moves available
+        :rtype: dict
+        """
         response = self.wool_request("progress-dialogue", qs={"replyId": moveID})
         return self.get_moves_from_response(response["value"])
 
     def get_moves_from_response(self, response):
+        """
+        Gets the set of moves from a Wool response
+
+        :param response: the response from Wool
+        :type response: dict
+        :return: the moves
+        :rtype: dict
+        """
         to_return = {"moves": {}, "replies": []}
 
         if response is not None:
