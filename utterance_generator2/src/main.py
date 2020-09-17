36d1918a28a785d6d655b1ea6497ade3730fcd2d
Mark Snaith <msnaith@dundee.ac.uk>
2020-09-17 21:30:13 +0100

    Bug fix for dialogue tracking

--- utterance_generator2/src/main.py
+++ utterance_generator2/src/main.py
@@ -11,12 +11,20 @@ def check_first_run():
 
         db = mongo.get_db()
 
-        collections = ["argument_models", "dictionary", "content_descriptors", "variables"]
+        collections = ["argument_models", "dictionary", "content_descriptors", "variables", "dialogue_topics"]
 
         for c in collections:
             with open(file.replace("{file}",c), "r") as f:
                 f = json.load(f)
-                db[c].insert_one(f)
+
+                if type(f) is list:
+                    for r in f:
+                        db[c].insert_one(r)
+                else:
+                    db[c].insert_one(f)
+
+        db["dialogues"].insert_one({}) # creates dialogues collection
+
 
 if __name__ == '__main__':
     check_first_run()
