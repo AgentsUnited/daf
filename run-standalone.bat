c2837fb96bfd75cd3725aefb46a3597799371c2f
Mark Snaith <msnaith@dundee.ac.uk>
2021-04-07 11:26:36 +0100

    Some config update

--- run-standalone.bat
+++ run-standalone.bat
@@ -1,5 +1,4 @@
 set COMPOSE_CONVERT_WINDOWS_PATHS=1
 
-
-docker-compose -f docker-compose.yml build --no-cache --build-arg demo=True
+docker-compose -f docker-compose.yml build --no-cache --build-arg demo=True --build-arg standalone=True
 docker-compose -f docker-compose.yml up --remove-orphans && docker-compose -f docker-compose.yml down --remove-orphans
